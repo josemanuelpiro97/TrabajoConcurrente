66e61948becbeb90a5137328f0cbac01571263ea
jose manuel <josemanuelpiro@gmail.com>
2020-06-06 19:43:58 -0300

    add same test

---
+++ Test/Monitor/Queue/QueueManagmentTest.java
@@ -0,0 +1,20 @@
+package Monitor.Queue;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueueManagmentTest {
+
+    @Test
+    void sleepN() {
+    }
+
+    @Test
+    void wakeN() {
+    }
+
+    @Test
+    void isSleep() {
+    }
+}
