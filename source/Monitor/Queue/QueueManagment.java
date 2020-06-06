6547209d485e19808b3d0c09bb675f4601d3d244
jose manuel <josemanuelpiro@gmail.com>
2020-06-06 19:32:32 -0300

    Queue body addition

--- source/Monitor/Queue/QueueManagment.java
+++ source/Monitor/Queue/QueueManagment.java
@@ -1,4 +1,74 @@
 package Monitor.Queue;
 
+import java.util.concurrent.Semaphore;
+
 public class QueueManagment {
+
+    /**
+     * Array of semaphores that represent all transition.
+     */
+    private Semaphore semaphores[];
+    private boolean sleepT [];
+
+    /**
+     * @brief QueueManagment constructor
+     * @param arraySize Numbre of transitions in the Petri net
+     */
+    public QueueManagment(int arraySize) {
+        this.semaphores = new Semaphore[arraySize];
+        this.sleepT = new boolean[arraySize];
+
+        for (int i = 0; i < arraySize; i++) {
+            this.semaphores[i] = new Semaphore(0);
+            this.sleepT[i] = false;
+        }
+    }
+
+
+    /**
+     * @param index [in]
+     * @brief put thread to sleep in its semaphore
+     */
+    public void sleepN(int index) {
+        for (int i = 0; i < this.semaphores.length; i++) {
+            if (i == index)
+                try {
+                    this.sleepT[i] = true;
+                    this.semaphores[i].acquire();
+                } catch (Exception e) {
+                }
+        }
+    }
+
+    /**
+     * @param index [in]
+     * @brief Wake thread of specific semaphore
+     */
+    public void wakeN(int index) {
+        for (int i = 0; i < this.semaphores.length; i++) {
+            if (i == index)
+                try {
+                    this.sleepT[i] = false;
+                    this.semaphores[i].release();
+                } catch (Exception e) {
+                }
+        }
+    }
+
+    /**
+     * @brief Check which thread are sleep
+     * @param sensitizedT [in]
+     * @return Array whit sensitive thread that are sleep
+     */
+    public boolean[] isSleep(boolean [] sensitizedT) {
+        boolean result[] = new boolean[sensitizedT.length];
+
+        for(int i=0;i<sensitizedT.length;i++){
+            result[i] = this.sleepT[i] && sensitizedT[i];
+        }
+
+        return result;
+    }
+
+
 }
