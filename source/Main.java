b1fdc67c028a39a155f564f326d9e8cc837c29f5
jose manuel <josemanuelpiro@gmail.com>
2020-06-13 21:10:17 -0300

    task and main ended

--- source/Main.java
+++ source/Main.java
@@ -1,11 +1,26 @@
+import Monitor.Monitor;
+import Monitor.Task;
+
+
 /**
  * @TODO Solo implementar la instancia de monitor.
  * @TODO implementar la clase Log para llevar el registro del programa
  **/
 
 public class Main {
+    public void main (String[] arg){
+        //build monitor
+        Monitor monitor = new Monitor();
 
-
+        //build and start, tasks and threads
+        Task[] tasks = new Task[4];
+        Thread[] threads = new Thread[4];
+        for (int i =0 ; i< 4 ; i++){
+            tasks[i] = new Task(i,monitor);
+            threads[i] = new Thread(tasks[i]);
+            threads[i].start();
+        }
+    }
 }
 
 
--- source/Monitor/Task.java
+++ source/Monitor/Task.java
@@ -1,24 +1,27 @@
 package Monitor;//Test Class of Monitor.Task
 
-public class Task  implements Runnable{
-    private Thread thread;
-    private int[] transitionN;
+public class Task implements Runnable {
+    private int transitionN;
     private Monitor monitor;
 
-    public Task(Thread thread,int[] transitionN,Monitor monitor) {
-        this.thread = thread;
+    public Task( int transitionN, Monitor monitor) {
         this.transitionN = transitionN;
         this.monitor = monitor;
     }
 
     @Override
-    public void run(){
+    public void run() {
         //test variable
         final int FINAL = 100;
 
-        for (int i =0;i<FINAL;i++){
+        for (int i = 0; i < FINAL; i++) {
             this.monitor.takeMonitor();
-          //  this.monitor.operate(this.transitionN);
+            try {
+                this.monitor.operate(this.transitionN);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
 }
